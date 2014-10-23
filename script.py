45e0f0905a358a9e7645182abddc76e7daa8632e
Mehdi <mehdi.rostaing@eleve.egd.mg>
2014-10-23 17:12:34 +0300

    Update script.py

--- script.py
+++ script.py
@@ -1,10 +1,11 @@
+#voilà le convertisseur nombre binaire en nombre décimal
 bi=input("Entrer nombre binaire: ")
 dec,i=0,0
 while i<len(bi):
-    rang=int(bi[i])
-    if rang==1:
-        dec=dec+2**(len(bi)-(i+1))
-    else:
+    rang=int(bi[i]) #rang des chiffres du nombre binaire
+    if rang==1: #si le chiffre est 1
+        dec=dec+2**(len(bi)-(i+1)) #va convertir le chiffre binaire en chiffre décimal selon son rang et va additioner les décimaux
+    else: #si le chiffre n'est pas 1 (dans ce cas là 0)
         dec+=0
     i+=1
-print(bi,"=",dec)
+print(bi,"(base 2)est égale à",dec,"(base 10)")
