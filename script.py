af136345d36085daf05ed15d39215315aafb817e
Mehdi <mehdi.rostaing@eleve.egd.mg>
2014-10-23 17:02:45 +0300

    Update script.py

--- script.py
+++ script.py
@@ -1,2 +1,10 @@
-#le script du convertisseur
-a=input("Entrer nombre binaire= ")
+bi=input("Entrer nombre binaire: ")
+dec,i=0,0
+while i<len(bi):
+    rang=int(bi[i])
+    if rang==1:
+        dec=dec+2**(len(bi)-(i+1))
+    else:
+        dec+=0
+    i+=1
+print(bi,"(base 2)est égale à",dec,"(base 10)")
