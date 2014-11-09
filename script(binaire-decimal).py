0e58ce8c3484a430b118e55a02b71b572dc15bb5
MickaelISN <mickael.ranivo@eleve.egd.mg>
2014-11-09 17:17:41 +0100

    Update script(binaire-decimal).py

--- script(binaire-decimal).py
+++ script(binaire-decimal).py
@@ -1,13 +1,18 @@
+# -*-coding:Latin-1 -* #Pour directement excecuter le programme sur l'interpréteur de commandes en deux clic sur le fichier <<sous windows>>
+
+import os #on importe le os qui dispose de variables et de fonctions utiles pour dialoguer avec le système d'exploitation <<sous windows>>
+
 choix=input("Tapez B pour convertir un nombre binaire en décimal\nou D pour convertir un nombre décimal en binaire: ")
 if choix==str("B") or choix==str("b"): #script du convertisseur nombre binaire en nombre décimal
         try: #pour lever les exceptions
-                bi=int(input("Entrez un nombre binaire: "))
+                bi=input("Entrez un nombre binaire: ")
         except NameError:
                 print("Veuillez saisir un nombre")  # exception qui pourra être levé par Python si la valeur n'a pas été définie
         except ValueError:
                 print("Veuilez saisir un nombre")   #exception qui pourra être levé par Python face à diverses erreurs de « valeurs »
-        dec,i=0,0
-        while i<len(bi):
+        dec=0
+        i=0
+        while i<int(len(bi)):
                 rang=int(bi[i]) #rang des chiffres du nombre binaire
                 if rang==1: #si le chiffre est 1
                         dec=dec+2**(len(bi)-(i+1)) #va convertir le chiffre binaire en chiffre décimal selon son rang et va additioner les décimaux
@@ -25,3 +30,5 @@ elif choix==str("D") or choix==str("d"): #script du convertisseur nombre décima
         print(dec,"vaut",bin(dec)[2:],"en binaire") #bin est la fonction qui permet de convertir un nombre décimal et binaire
 else:
         print("Veuillez saisir D ou B")
+
+os.system("pause") #On met le programme en pause pour éviter que l'interpréteur de commandes se ferme après l'excecution
