0f61a9befb2980edbb5cd6b088457bf20c4b1575
Ramiky <mikdad.goulamaly@eleve.egd.mg>
2014-11-12 11:18:52 +0400

    Update script(binaire-decimal).py

--- script(binaire-decimal).py
+++ script(binaire-decimal).py
@@ -23,6 +23,20 @@ elif choix==str("D") or choix==str("d"): #script du convertisseur nombre décima
                 print("Veuillez saisir un nombre")  # exception qui pourra être levé par Python si la valeur n'a pas été définie
         except ValueError:
                 print("Veuilez saisir un nombre")   #exception qui pourra être levé par Python face à diverses erreurs de « valeurs »
-        print(dec,"vaut",bin(dec)[2:],"en binaire") #bin est la fonction qui permet de convertir un nombre décimal et binaire
+        if dec<0:                                   #methode du complément à 2 , afin de pouvoir convertir en négatif
+                bi=bin(dec)
+                z=bi[3:]
+                for k in range(0,len(z)+1):  
+                        r=z[k]
+                        ch=""
+                        if int (r)="1":
+                                ch+"0"
+                        elif int(r)=="0":
+                                print(ch)
+
+        elif dec>0:
+            print(dec,"vaut",bin(dec)[2:],"en binaire") #bin est la fonction qui permet de convertir un nombre décimal et binaire
 else:
         print("Veuillez saisir D ou B")
+
+
